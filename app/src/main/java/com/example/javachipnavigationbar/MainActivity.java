ab99463dff08a6f41ded91250dded7364e0f4d36
riwltnchgo0625 <63505725+riwltnchgo0625@users.noreply.github.com>
2022-01-12 12:40:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/javachipnavigationbar/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.javachipnavigationbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    ChipNavigationBar bottomNav;
+    FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        if (savedInstanceState == null){
+            bottomNav.setItemSelected(R.id.home,true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, homeFragment)
+                    .commit();
+        }
+
+
+        bottomNav.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int id) {
+                Fragment fragment = null;
+                switch (id){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                    break;
+
+                    case  R.id.discover:
+                        fragment = new DiscoverFragment();
+                        break;
+
+                    case R.id.account:
+                        fragment = new AccountFragment();
+                        break;
+
+                    case R.id.start:
+                        fragment = new StartFragment();
+                        break;
+
+                    case R.id.record:
+                        fragment = new RecordFragment();
+                        break;
+                }
+
+                if (fragment!=null) {
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, fragment)
+                            .commit();
+                }else{
+                    Log.e(TAG,"Error in creating fragment");
+                }
+            }
+        });
+    }
+}
